78ea7e7a3cdd9bf4c5454f31de1ab5e24d394c42
KFU-user <KFU-user>
2016-10-06 11:33:00 +0300

    fixed filepath

--- SimpleEnterpriseMaven/src/main/java/ru/itis/Main.java
+++ SimpleEnterpriseMaven/src/main/java/ru/itis/Main.java
@@ -8,9 +8,7 @@ import ru.itis.service.SimpleUsersServiceImpl;
 public class Main {
 
     public static void main(String[] args) {
-        UsersDao usersDao = new UsersDaoFileBasedImpl("C:\\Users\\" +
-                "admin.WIN-IPM3OA3VQNQ\\Desktop" +
-                "\\JAVA\\JavaItis\\SimpleEnterprise\\users.txt");
+        UsersDao usersDao = new UsersDaoFileBasedImpl("C:\\Users\\KFU-user\\Desktop\\JavaItis\\SimpleEnterpriseMaven\\users.txt");
 
         SimpleUsersService service = new SimpleUsersServiceImpl(usersDao);
 
